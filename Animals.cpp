bf5ffe2398805b240c88bec86f98fcfa5b6b4873
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-06-30 03:24:21 +0400

    last commit of the Animals.cpp

--- Animals.cpp
+++ Animals.cpp
@@ -2,57 +2,90 @@
 //
 
 #include <iostream>
+#include <typeinfo>
+
 using namespace std;
 class Animal
-{
-private:
-    const string name = "It's an Animal";
+{ 
 public:
+    int weight;
+    string classname;
+
     Animal()
     {
-        cout << this->name << endl;
+        this->classname = typeid(*this).name() + string("  ");
     }
-    ~Animal()
+
+    bool operator<(const Animal& animal)
     {
-        cout << "Really it was an Animal" << endl;
+        if (this->weight < animal.weight)
+            return true;
+        else
+            return false;
     }
+    bool operator>(const Animal& animal)
+    {
+        if (this->weight > animal.weight)
+            return true;
+        else
+            return false;
+    }
+    bool operator==(const Animal& animal)
+    {
+        if (this->weight == animal.weight)
+            return true;
+        else
+            return false;
+    }
+
+    Animal& operator()()
+    { 
+        cout << this->classname << endl;
+        return *this;
+    }
+
+    virtual ~Animal(){}    
 };
 
 class Mammal : public Animal
 {
-private:
-    const string name = "It's a Mammal";
-public:
-    Mammal():Animal()
+public:   
+    Mammal()
     {
-        cout << this->name << endl;
+        this->classname = "  " + this->classname + typeid(*this).name() + "  ";
     }
-
-    ~Mammal()
-    {
-        cout << "Really it was a Mammal" << endl;
-    }
+    ~Mammal(){}   
 };
 
 class Lion : public Mammal
 {
-private:
-    const string name;
-public:
-    Lion(string name): name(name) , Mammal()
+public:   
+    Lion(int weight)
     {
-        cout << this->name << endl;
+        this->weight = weight;
+        this->classname = "  " + this->classname + typeid(*this).name() + "  ";
     }
-
-    ~Lion()
-    {
-        cout << "Really it was a Lion" << endl;
-    }
+    
+    ~Lion(){}    
 };
 
 int main()
 {
-    Lion lion("It isn't Elephant");
+    Lion lion1(500);
+    Lion lion2(700);
+    //Animal* lion1 = new Lion(900);
+    //Animal* lion2 = new Lion(500);
+
+    if(lion1 < lion2) 
+        std::cout << "The weight of the Lion1 less than the weight of the Lion2" << std::endl;
+    
+    if(lion1 > lion2)
+        std::cout << "The weight of the Lion1 more than the weight of the Lion2" << std::endl;
+
+    if(lion1 == lion2)
+        std::cout << "The weight of the Lion1 is equal to the weight of the Lion2" << std::endl;
+
+    lion1();
 
     return 0;
 }
